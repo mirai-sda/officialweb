5f3238e5cbebaf1f722a6082497d9c77b517f6a6
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2026-02-17 07:11:45 -0300

    update technology:nextjs and pnpm

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base:"https://thecoderbutcher.github.io/miraidev-officialweb",
-  plugins: [react()],
-})
